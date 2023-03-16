9e3af4bcf9b5d76ffc1335898b7f084b63c1ba7e
badgertista <45010887+badgertista@users.noreply.github.com>
2023-03-16 14:35:09 -0400

    Add files via upload

--- deploy_trend_micro.sh
+++ deploy_trend_micro.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Telechargement du fichier .pkg
-sudo curl -LO https://github.com/badgertista/intunetest/raw/main/endpoint_basecamp.pkg
+sudo curl -L -O https://github.com/badgertista/intunetest/raw/main/endpoint_basecamp.pkg
 
 # Installation
-sudo installer -pkg Endpoint_Basecamp_MAC.pkg -target /
+sudo installer -pkg Endpoint_Basecamp.pkg -target /
 
 # Retrait du fichier d'installation
-sudo rm Endpoint_Basecamp_MAC.pkg
+sudo rm Endpoint_Basecamp.pkg
 
 # Signal Intune that the installation is complete
 # sudo jamf policy -trigger intune_app_installed
